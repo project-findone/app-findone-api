50fc905f187d269b57c34f4b1ac574e0eac5f131
Izaias Henrique <izaias.rique@gmail.com>
2022-08-13 22:48:27 -0300

    chore: add and configure eslint

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import express from "express";
-const app = express();
+import express from 'express'
+const app = express()
 
-const port = 8080;
+const port = 8080
 
 app.get('/', (request, response) => {
-    return response.json({message: 'Application is ready!'});
+  return response.json({ message: 'Application is ready!' })
 })
 
 app.listen(port, () => {
-    console.log(`😎 Server is running at port: ${port}`);
+  console.log(`😎 Server is running at port: ${port}`)
 })
