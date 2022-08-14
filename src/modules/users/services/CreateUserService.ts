b9b503ae8aa621223c15427f4d0f9388caf5b232
Izaias Henrique <izaias.rique@gmail.com>
2022-08-14 19:26:11 -0300

    feat: add CreateUserService

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,17 @@
+import { ICreateUserDTO } from '../dtos/ICreateUserDTO'
+import { User } from '../infra/prisma/entities/User'
+import { ICreateUserRepository } from '../repositories/ICreateUserRepository'
+
+interface IRequest {
+  data: ICreateUserDTO
+}
+
+class CreateUserService {
+  constructor (private createUserRepository: ICreateUserRepository) {}
+  async handle (request: IRequest): Promise<User> {
+    const user = await this.createUserRepository.create(request.data)
+    return user
+  }
+}
+
+export default CreateUserService
