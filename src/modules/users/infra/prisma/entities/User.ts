6f08a2a88dc454a9968487392e41de8fe95d6423
Izaias Henrique <izaias.rique@gmail.com>
2022-08-18 22:05:43 -0300

    refactor(Entity): refactor the User Entity with new property names

--- src/modules/users/infra/prisma/entities/User.ts
+++ src/modules/users/infra/prisma/entities/User.ts
@@ -1,27 +1,26 @@
-import { tb_pessoa } from '@prisma/client'
+import { Person } from '@prisma/client'
 
-export class User implements tb_pessoa {
-  public PESSOA_TIPO_ID_FK!: number
-  public TB_PESSOA_DATA_NASC!: Date | null
-  public TB_PESSOA_CPF!: number | null
-  public TB_PESSOA_IDADE!: number | null
-  public TB_PESSOA_SEXO!: string | null
-  public TB_PESSOA_EMAIL!: string | null
-  public TB_PESSOA_SENHA!: string | null
-  public TB_PESSOA_TEL!: string | null
-  public TB_PESSOA_CEP!: number | null
-  public ESTADO_ID_FK!: number | null
-  public TB_PESSOA_CIDADE!: string | null
-  public TB_PESSOA_BIO!: string | null
-  public TB_PESSOA_PONTOS!: number | null
-  public PESSOA_RESPONSAVEL_FK!: number | null
-  public TB_PESSOA_DEFIENCIA!: string | null
-  public TB_PESSOA_PARENTESCO!: string | null
-  public TB_PESSOA_IMAGEM!: Buffer | null
-  public TB_PESSOA_CARACTERISTICA_AD!: string | null
-  public TB_PESSOA_ID!: number
-  public TB_PESSOA_NOME!: string
-  public TB_PESSOA_SOBRENOME!: string | null
+export class User implements Person {
+  public person_id!: number
+  public personType_id!: number
+  public name!: string
+  public lastname!: string
+  public birth_date!: Date | null
+  public personCPF!: string | null
+  public age!: number | null
+  public gender!: string | null
+  public email!: string | null
+  public password!: string | null
+  public phone_number!: string | null
+  public personCEP!: number
+  public state_id!: number
+  public city!: string
+  public person_biografy!: string | null
+  public score!: number | null
+  public owner_id!: number | null
+  public person_deficiency!: string | null
+  public person_kinship!: string | null
+  public person_image!: string | null
 
   constructor (props: User) {
     Object.assign(this, props)
