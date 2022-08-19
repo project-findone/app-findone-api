0fcc46fe2545df556f04fbbf916cff64ea5199d0
Izaias Henrique <izaias.rique@gmail.com>
2022-08-18 22:16:47 -0300

    refactor(DTO): refactor ICreateUserDTO with correct params names

--- src/modules/users/dtos/ICreateUserDTO.ts
+++ src/modules/users/dtos/ICreateUserDTO.ts
@@ -1,24 +1,18 @@
 
 export interface ICreateUserDTO {
-  TB_PESSOA_ID: number
-  PESSOA_TIPO_ID_FK: number
-  TB_PESSOA_NOME: string
-  TB_PESSOA_SOBRENOME: string | null
-  TB_PESSOA_DATA_NASC: Date | null
-  TB_PESSOA_CPF: number | null
-  TB_PESSOA_IDADE: number | null
-  TB_PESSOA_SEXO: string | null
-  TB_PESSOA_EMAIL: string
-  TB_PESSOA_SENHA: string | null
-  TB_PESSOA_TEL: string | null
-  TB_PESSOA_CEP: number | null
-  ESTADO_ID_FK: number | null
-  TB_PESSOA_CIDADE: string | null
-  TB_PESSOA_BIO: string | null
-  TB_PESSOA_PONTOS: number | null
-  PESSOA_RESPONSAVEL_FK: number | null
-  TB_PESSOA_DEFIENCIA: string | null
-  TB_PESSOA_PARENTESCO: string | null
-  TB_PESSOA_IMAGEM: Buffer | null
-  TB_PESSOA_CARACTERISTICA_AD: string | null
+  personType_id: number
+  name: string
+  lastname: string
+  birth_date: Date
+  personCPF: string
+  gender: string
+  email: string
+  password: string
+  phone_number: string
+  personCEP: number
+  state_id: number
+  city: string
+  score: 0
+  person_biografy: string | null
+  person_image: string | null
 }
