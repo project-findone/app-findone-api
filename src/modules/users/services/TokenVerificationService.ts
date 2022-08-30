54b93e12a8a4716b1e3cafea57b9682f51f52fb9
Izaias Henrique <izaias.rique@gmail.com>
2022-08-30 00:04:03 -0300

    chore: add crypto-js dependencie

---
+++ src/modules/users/services/TokenVerificationService.ts
@@ -0,0 +1,20 @@
+import { sign } from 'jsonwebtoken'
+import AuthConfig from '@config/auth'
+
+interface IResponse {
+  token: string
+}
+
+export class TokenVerificationService {
+  public async handle (email: string): Promise<IResponse> {
+    const { JWT } = AuthConfig
+    const verifyCode = Math.floor(Math.random() * 10000) + 1000
+
+    const token = sign({ verifyCode }, JWT.JWTSecret, {
+      subject: email,
+      expiresIn: '240s'
+    })
+
+    return { token }
+  }
+}
