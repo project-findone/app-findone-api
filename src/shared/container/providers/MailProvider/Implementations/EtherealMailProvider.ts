34e7a2d98a4fd8d968bce81d947cd58a3a5803ec
Izaias Henrique <izaias.rique@gmail.com>
2022-08-29 23:06:46 -0300

    feat: add Ethereal Mail Provider implementation

--- src/shared/container/providers/MailProvider/Implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/Implementations/EtherealMailProvider.ts
@@ -1,7 +1,39 @@
+import nodemailer, { Transporter } from 'nodemailer'
 import { IMailProvider } from '../Models/IMailProvider'
 
 export class EtherealMailProvider implements IMailProvider {
-  constructor () {
+  private client!: Transporter
 
+  public async initialize (): Promise<void> {
+    const account = await nodemailer.createTestAccount()
+
+    const transporter = await nodemailer.createTransport({
+      host: account.smtp.host,
+      port: account.smtp.port,
+      secure: account.smtp.secure,
+      auth: {
+        user: account.user,
+        pass: account.pass
+      }
+    })
+
+    this.client = transporter
+  }
+
+  public async sendMail (to: string, body: string): Promise<void> {
+    if (!this.client) {
+      throw new Error(' O transportador precisa ser iniciado.')
+    }
+
+    const message = await this.client.sendMail({
+      from: ' Equipe FindOne <projetofindone@gmail.com> ',
+      to,
+      subject: 'Código de Verificação - FindOne',
+      text: body
+    })
+
+    console.log('Message sent: %s', message.messageId)
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message))
   }
 }
