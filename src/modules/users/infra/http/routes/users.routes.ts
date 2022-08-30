d2041f0ca30d30cd9f9175d83c0ec575bf785905
Izaias Henrique <izaias.rique@gmail.com>
2022-08-30 08:41:25 -0300

    feat: add token verification route

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -2,12 +2,20 @@ import CreateUserService from '@modules/users/services/CreateUserService'
 import { container } from 'tsyringe'
 import { Router } from 'express'
 
+import { TokenVerificationService } from '@modules/users/services/TokenVerificationService'
+
 const usersRouter = Router()
 
 usersRouter.post('/', async (request, response) => {
   const createUserService = container.resolve(CreateUserService)
   const user = await createUserService.handle(request.body)
   return response.json({ message: user })
+})
+
+usersRouter.post('/verify', async (request, response) => {
+  const tokenVerificationService = container.resolve(TokenVerificationService)
+  const token = await tokenVerificationService.handle(request.body)
+  return response.json(token)
 })
 
 export default usersRouter
