63c485b4574f411cd14b492ffdc310c2c680d27d
Izaias Henrique <izaias.rique@gmail.com>
2022-08-30 08:39:35 -0300

    feat: add token verification service

--- src/modules/users/services/TokenVerificationService.ts
+++ src/modules/users/services/TokenVerificationService.ts
@@ -1,22 +1,51 @@
 import { sign } from 'jsonwebtoken'
-import {} from 'crypto-js/aes'
+import { encrypt } from 'crypto-js/aes'
+import { inject, injectable } from 'tsyringe'
 
 import AuthConfig from '@config/auth'
+import { IMailProvider } from '@shared/container/providers/MailProvider/Models/IMailProvider'
 
 interface IResponse {
   token: string
 }
 
+@injectable()
 export class TokenVerificationService {
+  constructor (
+    @inject('EtherealMailProvider')
+    private mailProvider: IMailProvider) {}
+
   public async handle (email: string): Promise<IResponse> {
-    const { JWT } = AuthConfig
-    const verifyCode = Math.floor(Math.random() * 10000) + 1000
+    const { JWT, AES } = AuthConfig
 
-    const token = sign({ verifyCode }, JWT.JWTSecret, {
-      subject: email,
-      expiresIn: '240s'
-    })
+    if (!email) {
+      throw new Error(' Nenhum e-mail foi encontrado.')
+    }
 
-    return { token }
+    const verifyCode = (Math.floor(Math.random() * 10000) + 1000).toString()
+
+    if (!AES.Secret) {
+      throw new Error('O AES Secret não foi encontrado.')
+    }
+
+    try {
+      const data = {
+        to: { email: JSON.stringify(email) },
+        body: verifyCode
+      }
+      await this.mailProvider.sendMail(data)
+
+      const codeEncrypted = encrypt(verifyCode, AES.Secret).toString()
+
+      const token = sign({ codeEncrypted }, JWT.Secret, {
+        subject: String(email),
+        expiresIn: 120 // 2 minutes
+      })
+
+      return { token }
+    } catch (e) {
+      throw new Error(`Houve um erro ao criar o token de verificação.
+        \n \n${e as string}`)
+    }
   }
 }
